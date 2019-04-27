a819284ee133d7d5c48738b6a20e0aee6146d8c2
1237 <mamedov_vs@oksw.ru>
2019-04-27 16:50:08 +0700

    Разбито на более мелкие функции. При вводе не виден пароль

--- password_strength.py
+++ password_strength.py
@@ -1,23 +1,42 @@
 import re
+from getpass import getpass
+
+
+def has_lower_case(password):
+    return bool(re.search(r'[a-z]', password))
+
+def has_upper_case(password):
+    return bool(re.search(r'[A-Z]', password))
+
+def has_numbers(password):
+    return bool(re.search(r'[0-9]', password))
+
+def has_special_characters(password):
+    return bool(re.search(r'\W', password))
+
+def has_number_phone(password):
+    return bool(re.search(r'\+?\d{1,3}?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d', password))
+
+def has_email(password):
+    return bool(re.search(r'[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}', password))
 
 
 def get_password_strength(password):
-    rezult = 0
 
-    for exp in [r'[a-z]', r'[A-Z]', r'[0-9]', r'[@,#,$]']:
-        rezult += 1 if re.search(exp, password) else 0
-    if re.search(r'.{8,}', password):
-        rezult += 2
-        for exp in [r'\+?\d{1,3}?[- .]?\(?(?:\d{2,3})\)'
-                    r'?[- .]?\d\d\d[- .]?\d\d\d\d',
-                    r'[0-9a-z-\.]+\@[0-9a-z-]'
-                    r'{2,}\.[a-z]{2,}',
-                    ]:
-            rezult += 2 if not re.search(exp, password) else 0
-    return rezult
+    result = len(password) >= 8
+    result += has_lower_case(password)
+    result += has_upper_case(password) * 2
+    result += has_numbers(password) * 2
+    result += has_special_characters(password) * 2
+    result += (not has_number_phone(password)) * 1
+    result += (not has_email(password)) * 1
+    return result
 
+def main():
+    max_bal = 10
+    password = getpass('Please enter your password: ')
+    result = get_password_strength(password)
+    print('Password complexity {} out of {}'.format(result, max_bal))
 
 if __name__ == '__main__':
-    password = input('Password: ')
-    rezult = get_password_strength(password)
-    print('Security', rezult)
+    main()
